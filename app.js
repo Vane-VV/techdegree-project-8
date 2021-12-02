1a8a8aeb380e03c23d8d1ec11b632ac0e27a5c06
Vanessa Velasco-Valles <2023vvelasco@arisehighschool.org>
2021-12-02 01:08:32 -0800

    added some css

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const url = `https://randomuser.me/api/`;
-console.log(fetch(url)
-           .then(response => response.json())
-           .then(obj => obj.results[0])
-           .then(result => result.email)
-           );
+
+
+
+
+
+
+
+
+//const url = `https://randomuser.me/api/`;
+//console.log(fetch(url)
+//           .then(response => response.json())
+//           .then(obj => obj.results[0])
+//           .then(result => result.email)
+//           );
