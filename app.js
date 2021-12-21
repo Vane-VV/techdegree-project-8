8d883082547c12717c05243561ce43c917389e64
Vanessa Velasco-Valles <2023vvelasco@arisehighschool.org>
2021-12-20 23:36:59 -0800

    added js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const main = document.querySelector('main');
+const url = `https://randomuser.me/api/`;
+const result = fetch(url)
+           .then(response => response.json())
+           .then(obj => obj.results[0])
+           .catch(err => `Oops! An error occurred: ${err}`);
 
-
-
-
-
+result.then(info => console.log(info.picture.medium));
 
 
 
@@ -12,3 +15,49 @@
 //           .then(obj => obj.results[0])
 //           .then(result => result.email)
 //           );
+
+
+
+//html = `<div  class="card">
+//      <img  class="avatar" src="${result.then(info => info.picture.large)}">
+//      <div class="text-container">
+//          <h2 class="name">First - Last</h2>
+//          <p class="email">something.gmail.com</p>
+//          <p class="adress">City</p>
+//      </div>
+//    </div>`;
+
+//main.innerHTML = html;
+
+
+
+//const addTwo = (n) => n + 2;
+
+
+
+//function addProfile(profile) {
+//  const div = document.createElement('div');
+//  div.style.display = 'none';
+//  div.className = 'card';
+//  profile.then(info => makeCardImage(info, div));
+//}
+//
+//
+
+
+
+//const picture = result.then(result => result.picture.large)
+//const html = `<div  class="card">
+//      <img  class="avatar" src="${result.picture.large}">
+//      <div class="text-container">
+//          <h2 class="name">First - Last</h2>
+//          <p class="email">something.gmail.com</p>
+//          <p class="adress">City</p>
+//      </div>
+//    </div> `
+
+
+
+//main.innerHTML = html;
+
+
