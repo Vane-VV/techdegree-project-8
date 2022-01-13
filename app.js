915a2b098a0f628aa486b81ac6ae19c9e2ac01c2
Vanessa Velasco-Valles <2023vvelasco@arisehighschool.org>
2022-01-12 19:37:35 -0800

    added the street number and street name

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+
+// global variables
 let employees = [];
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,email, location, phone, dob &noinfo &nat=US`
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
 
+
+// fetch data from API
 fetch(urlAPI)
 .then(res => res.json())
 .then(res => res.results)
@@ -12,14 +16,20 @@ fetch(urlAPI)
 .catch(err => console.log(err))
 
 function displayEmployees(employeeData) {
+  
     employees = employeeData;
+  
+    // store the employee HTML as we create it
     let employeeHTML = '';
+  
+    // loop through each employee and create HTML markup
     employees.forEach((employee, index) => {
     let name = employee.name;
     let email = employee.email;
     let city = employee.location.city;
     let picture = employee.picture;
       
+    // template literals make this so much cleaner!
     employeeHTML += `
       <div class="card" data-index="${index}">
         <img class="avatar" src="${picture.large}" />
@@ -37,7 +47,9 @@ function displayEmployees(employeeData) {
 
 function displayModal(index) {
   
+    // use object destructuring make our template literal cleaner
     let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
+  
     let date = new Date(dob.date);
 
     const modalHTML = `
@@ -48,15 +60,16 @@ function displayModal(index) {
         <p class="address">${city}</p>
         <hr />
         <p>${phone}</p>
-        <p class="address">${street}, ${state} ${postcode}</p>
+        <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
         <p>Birthday:
         ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
         </div>
     `;
-
+    
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
    }
+
 
     gridContainer.addEventListener('click', e => {
 
@@ -67,6 +80,8 @@ function displayModal(index) {
       }
     });
 
+
     modalClose.addEventListener('click', () => {
         overlay.classList.add("hidden");
         });
+
